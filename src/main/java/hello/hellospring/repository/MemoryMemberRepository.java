694e99f362642443f5eea23869788274b55d87d2
moonjiyoon <ske0830@gmail.com>
2022-05-09 17:30:57 +0900

    create Member domain and repository

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    // 실무에서는 동시성 문제 생각해야 하지만 예제라서 간단히 구현?
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;  // key 값 생성
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // 값이 null 일 수 있으니 Optional.ofNullable 로 감싸서 return
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
